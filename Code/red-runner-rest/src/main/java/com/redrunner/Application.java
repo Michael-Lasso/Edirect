ce0b701c45374a507fd887893a65049a6db398fc
Michael Lasso <lassomicha@hotmail.com>
2017-03-21 19:35:36 -0400

    Rest module functional with mybatis framework.

--- Code/red-runner-rest/src/main/java/com/redrunner/Application.java
+++ Code/red-runner-rest/src/main/java/com/redrunner/Application.java
@@ -16,8 +16,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 
